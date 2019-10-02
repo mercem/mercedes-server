e2b1d0176c6b9afeea9414999f1434c4283b4aa0
Omer Cem Akyol <omercema@gmail.com>
2019-10-02 10:49:59 +0300

    Read me added

---
+++ README.md
@@ -0,0 +1,1 @@
+Add your own Client ID and Client Secret to ./app/config/environment.ts
--- app/config/environment.ts
+++ app/config/environment.ts
@@ -1,3 +1,3 @@
 export let redirectUri = 'http://localhost:3001'; // front-end url
-export let ClientID = '57778c5b-2b57-45c5-9260-c830058e5d53';
-export let ClientSecret = 'ed3c75fc-7980-4deb-bf45-2c3680439dfc';
+export let ClientID = 'Change Here';
+export let ClientSecret = 'Change Here';
